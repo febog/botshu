b425b71837782dc265b1fe8d8108b6bb53e6b1d5
Felipe Bojorquez <felipe@febog.com>
2023-04-18 21:07:21 -0700

    Replace tmi.js with Twurple

--- index.js
+++ index.js
@@ -2,37 +2,59 @@
 // Felipe Bojorquez
 // 2021
 require("dotenv").config();
-const tmi = require("tmi.js");
+const { RefreshingAuthProvider } = require("@twurple/auth");
+const { ChatClient } = require("@twurple/chat");
+const storage = require("./lib/storage/storage.js");
 const store = require("./lib/global-bot-state.js");
 const server = require("./lib/server/app.js");
 const lang = require("./lib/language/language.js");
 const parametersClient = require("./lib/commands/common/handler-parameter.js");
 const messageHandlers = require("./lib/commands/message-handlers.js");
 
-const client = new tmi.Client({
-    connection: {
-        secure: true,
-        reconnect: true,
-    },
-    identity: {
-        username: "modshu",
-        password: process.env.CHAT_ACCESS_TOKEN,
-    },
-    channels: store.isProduction() ? ["mushu", "febog"] : ["febog"],
-});
+async function startBotshu() {
+    // Prepare Twurple authentication provider
+    const clientId = process.env.TWITCH_CLIENT_ID;
+    const clientSecret = process.env.TWITCH_CLIENT_SECRET;
+    const tokenData = await storage.readJsonFromBlobStorage("token-data.json");
+    const authProvider = new RefreshingAuthProvider({
+        clientId,
+        clientSecret,
+        onRefresh: async (userId, newTokenData) =>
+            await storage.storeJsonToBlobStorage(
+                newTokenData,
+                "token-data.json"
+            ),
+    });
 
-// Gets called every time a new message arrives to the chat
-client.on("message", async (channel, user, message, self) => {
-    // If the message comes from the bot itself, ignore.
-    if (self) return;
+    await authProvider.addUserForToken(tokenData, ["chat"]);
 
-    parametersClient.setParameters(client, channel, user, message, store);
-    let p = parametersClient.getParameters();
+    // Connect to chat
+    const channels = store.isProduction() ? ["mushu", "febog"] : ["febog"];
+    const chatClient = new ChatClient({ authProvider, channels });
 
-    messageHandlers.handleMessage(p);
+    chatClient.onMessage(async (channel, user, text, msg) => {
+        // If the message comes from the bot itself, ignore.
+        if (user === "modshu") return;
 
-    await lang.handleMessageLanguage(p);
-});
+        parametersClient.setParameters(
+            chatClient,
+            channel,
+            user,
+            text,
+            store,
+            msg
+        );
+        let p = parametersClient.getParameters();
 
-server.initializeBotServer(store);
-client.connect();
+        messageHandlers.handleMessage(p);
+
+        await lang.handleMessageLanguage(p);
+    });
+
+    chatClient.connect();
+
+    // Start express for the bot website
+    server.initializeBotServer(store);
+}
+
+startBotshu();
