00ef05cde3fd59e9c3f4dca4c7e00fbccfb59a44
Felipe Bojorquez <felipe@febog.com>
2026-05-02 19:48:39 -0700

    Add root ES module and initial implementation

---
+++ src/web/routes/rank.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var router = express.Router();
+
+const VALORANT_API_KEY = process.env.VALORANT_API_KEY;
+
+async function getRank(res) {
+    const valorantApiResponse = await fetch("https://api.henrikdev.xyz/valorant/v1/mmr/eu/Mushu/2725?api_key=" + VALORANT_API_KEY);
+    const valorantApiData = await valorantApiResponse.json();
+    const rankTier = valorantApiData?.data?.currenttierpatched;
+    const rankRating = valorantApiData?.data?.ranking_in_tier;
+    // Form command response as "Diamond 3 - 71RR"
+    const fullRank = `${rankTier} - ${rankRating}RR`;
+    res.send(fullRank);
+}
+
+module.exports = {
+    getRouter() {
+        /* GET queue page. */
+        router.get("/", function (req, res, next) {
+            getRank(res);
+        });
+
+        return router;
+    },
+};
